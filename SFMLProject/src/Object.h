b80f6da925694932654f710554882d7bad791432
Brad <92309314+Brad0408@users.noreply.github.com>
2024-03-22 11:25:48 +0000

    uuid

--- SFMLProject/src/Object.h
+++ SFMLProject/src/Object.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <UUID.h>
 
-class UUID;
 
 class Object
 {
@@ -12,7 +12,7 @@ public:
 	//Copy Constructor, copies uuid from the source object to the destination object
 	Object(const Object&) = default;
 
-	UUID uuid;
+	AG::UUID uuid;
 
 	//Compares two Object instances, compares the uuids by casting them to a uint_64, returns true if two Objects are equal
 	inline bool operator==(const Object& rhs) { return (uint64_t)this->uuid == (uint64_t)rhs.uuid; }
